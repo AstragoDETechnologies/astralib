3cda868cb8256d9ca4eb382d1c6a3414a2ba894a
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-12-02 22:24:09 +0100

    initial commit

---
+++ src/io.rs
@@ -0,0 +1,17 @@
+mod fs;
+
+use std::{error::Error, io::Write};
+#[allow(unused)]
+pub fn input(prompt: &str) -> Result<String, Box<dyn Error>> {
+    print!("{}", prompt);
+    match std::io::stdout().flush() {
+        Err(_) => {
+            println!("");
+        }
+        _ => (),
+    }
+    let mut input: String = String::new();
+    std::io::stdin().read_line(&mut input)?;
+    input = input.trim().to_string();
+    Ok(input)
+}
