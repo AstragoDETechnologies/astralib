7fede6bcdbd4686a6dca3d1fb2137928047c0d9a
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-12-04 08:55:37 +0100

    Add debug logs

--- src/io.rs
+++ src/io.rs
@@ -1,5 +1,7 @@
 //! This module is used to perform input and output operations.
 
+use tracing::*;
+
 pub mod fs;
 
 use std::{error::Error, io::Write};
@@ -7,15 +9,19 @@ use std::{error::Error, io::Write};
 /// Prompts for an input on `stdin` and returns it in a result.
 #[allow(unused)]
 pub fn input(prompt: &str) -> Result<String, Box<dyn Error>> {
+    debug!("Printing prompt.");
     print!("{}", prompt);
     match std::io::stdout().flush() {
         Err(_) => {
+            error!("Could not flush stdout buffer. Printing newline.");
             println!("");
         }
         _ => (),
     }
+    debug!("Reading input to String...");
     let mut input: String = String::new();
     std::io::stdin().read_line(&mut input)?;
+    debug!("Trimming input.");
     input = input.trim().to_string();
     Ok(input)
 }
--- src/io/fs.rs
+++ src/io/fs.rs
@@ -1,11 +1,14 @@
 //! This submodule is used for io operations on the file system.
 
+use tracing::*;
+
 use std::path::Path;
 use std::{error::Error, io::Write};
 
 /// Reads a file as String.
 #[allow(unused)]
 pub fn read_file(path: &str) -> Result<String, Box<dyn Error>> {
+    debug!("Reading contents of {} to String.", path);
     let contents: String = std::fs::read_to_string(path)?;
     Ok(contents)
 }
@@ -13,6 +16,7 @@ pub fn read_file(path: &str) -> Result<String, Box<dyn Error>> {
 /// Writes a file as String.
 #[allow(unused)]
 pub fn write_file(path: &str, contents: &str) -> Result<(), Box<dyn Error>> {
+    debug!("Writing &str contents to {}.", path);
     let mut file = std::fs::File::create(Path::new(path))?;
     file.write_all(contents.as_bytes())?;
     Ok(())
